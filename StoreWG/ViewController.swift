d54975085538bbd71235845d7fdd278ee796666e
Jorge Luis Rivera Ladino <riveraladinojorgeluis>
2020-04-28 16:19:17 -0500

    Initial Commit

---
+++ StoreWG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreWG
+//
+//  Created by jennifer hasblady anzola ladino on 28/04/20.
+//  Copyright © 2020 Jorge Luis Rivera Ladino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
